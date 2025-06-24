766cf954992c50d41c1b47f13c253a6e389baac7
squareball <sonicrider222@gmail.com>
2025-06-25 00:12:22 +0500

    initial commit

---
+++ src/test/java/com/example/mywebstore/storefront/StorefrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mywebstore.storefront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorefrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
